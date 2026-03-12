cec9d303ec4757bb8a57000b7554b2f7750bd1de
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2026-03-12 20:35:50 +0300

    added the Menu page

---
+++ src/app/pages/menu/menu.page.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-menu-page',
+  templateUrl: './menu.page.html',
+  styleUrl: './menu.page.css'
+})
+export class MenuPage {
+  readonly menuTitle = 'Основное меню';
+}
