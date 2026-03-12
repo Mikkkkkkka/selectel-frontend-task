9b71f66822322ba730901de29a2418ebe6948131
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2026-03-12 20:37:17 +0300

    added page routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home/home.page';
+import { MenuPage } from './pages/menu/menu.page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePage, title: 'Главная' },
+  { path: 'menu', component: MenuPage, title: 'Меню' },
+  { path: '**', redirectTo: '' }
+];
